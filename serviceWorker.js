fc88a68216de909879a0d3055aaf043532f50d0b
17699A0532 <51587775+17699A0532@users.noreply.github.com>
2019-06-26 14:47:03 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,8 +3,26 @@ self.addEventListener("install",(event)=>{
   event.waitUntil(
     caches.open("sample").then((cache)=>{
     return cache.addAll([
-
-      ]);
+      './',
+			'./index.html',
+			'./restaurant.html',
+			'./serviceWorker.js',
+			'./js/dbhelper.js',
+			'./js/main.js',
+			'./js/restaurant_info.js',
+			'./css/styles.css',
+			'./data/restaurants.json',
+			'./img/1.jpg',
+			'./img/2.jpg',
+			'./img/3.jpg',
+			'./img/4.jpg',
+			'./img/5.jpg',
+			'./img/6.jpg',
+			'./img/7.jpg',
+			'./img/8.jpg',
+			'./img/9.jpg',
+			'./img/10.jpg'
+    ]);
     }));
 });
 
